cf69cdea909e7006fc932a9c009eec25be5d7525
Gesiel Gimenez <gesielgimenez@gmail.com>
2023-10-17 15:21:19 -0400

    test_button

--- src/components/front/Book.jsx
+++ src/components/front/Book.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import "./../scss/Book.scss";
 
 const Book = () => {
   return (
+    
+    <>
     <div>Book</div>
-  )
+
+    <div className="mainBook">
+      
+    
+    <div className="mainBook_one">Campus One</div>
+    <div className="mainBook_two">Campus Two</div>
+    
+    </div>
+    </>
+    )
 }
 
 export default Book
